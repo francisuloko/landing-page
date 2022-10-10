acdb2f3ffcb7d47e414bf4662a1a740b724c5a43
francisuloko <francisuloko@yahoo.com>
2022-10-10 22:27:12 +0100

    Change to ga4

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import {
 import Landing from "./pages/Landing.js";
 import NotFound from "./pages/NotFound.js";
 import Optin from "./pages/Optin.tsx";
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 const theme = createTheme();
-ReactGA.initialize("G-9V9JMCS6CH")
+const ID = "G-9V9JMCS6CH";
+ReactGA.initialize(ID)
+ReactGA.send("pageview");
 
 function App() {
   return (
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,7 @@
-import {
-  useEffect
-} from "react";
+import { useEffect } from "react";
 import Hero from "../components/Hero.js";
 import Opening from "../components/Opening.js";
-import Projects from '../components/Projects.js'
+import Projects from "../components/Projects.js";
 import Argument from "../components/Argument.js";
 import Appointment from "../components/Appointment.js";
 import Process from "../components/Process.js";
@@ -11,41 +9,31 @@ import Testimonials from "../components/Testimonials.js";
 import FAQ from "../components/FAQ.js";
 import Footer from "../components/Footer.js";
 import "../assets/fonts/AlfaSlabOne-Regular.ttf";
-import ReactGA from 'react-ga'
+import ReactGA from "react-ga";
 
 export default function Landing() {
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname)
-  }, [])
-  return ( <
-    >
-    <
-    Hero / >
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+  return (
+    <>
+      <Hero />
 
-    <
-    Opening / >
+      <Opening />
 
-    <
-    Projects / >
+      <Projects />
 
-    <
-    Argument / >
+      <Argument />
 
-    <
-    Process / >
+      <Process />
 
-    <
-    Testimonials / >
+      <Testimonials />
 
-    <
-    Appointment / >
+      <Appointment />
 
-    <
-    FAQ / >
+      <FAQ />
 
-    <
-    Footer / >
-    <
-    />
+      <Footer />
+    </>
   );
 }
