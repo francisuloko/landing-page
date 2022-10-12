f70b0c58684c074112d1c845cf567553765f5e20
francisuloko <francisuloko@yahoo.com>
2022-10-12 02:17:46 +0100

    Track button clicks

--- src/common/events.js
+++ src/common/events.js
@@ -3,7 +3,7 @@ import ReactGa from 'react-ga4';
 export function TrackClick(booked) {
   ReactGa.event({
     category: "CTA Button",
-    action: "Book Appointment",
+    action: "Clicked Book Appointment",
     label: "from " + booked,
   })
 }
